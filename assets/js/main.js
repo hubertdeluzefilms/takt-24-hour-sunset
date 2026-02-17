4e7af9d600979ba4055c9d7d74d63aec1106e665
hubertdeluzefilms <hubert.deluze.films@gmail.com>
2026-02-17 17:46:53 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+window.addEventListener("load", () => {
+  if (window.location.hash) {
+    const el = document.querySelector(window.location.hash);
+    if (el) el.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
+});
